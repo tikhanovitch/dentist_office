326b4613351b4cca9243539337686d82b5209969
tikhanovitch <tikhanovitch@gmail.com>
2024-10-08 14:20:09 +0300

    some changes
    Install:
    
    Create:

--- src/config/urls.py
+++ src/config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from patient_app import urls as patient_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('dentist/', include('patient_app.urls')),
+    # path('dentist/', include('patient_app.urls')),
+    path('dentist/', include(patient_urls)),
 ]
--- src/patient_app/urls.py
+++ src/patient_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import register
 from django.urls import include
+from . import views
 
 
 urlpatterns = [
     # path('some_url', some_view),
     path('register', register, name='register'),
-
+    # path('register/', views.register, name='register'),
 
 ]
